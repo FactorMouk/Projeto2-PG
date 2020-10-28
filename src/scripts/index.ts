9ed774439f2f5e308fcc28b34f69569b5eeb4937
FactorMouk <jvoa@cin.ufpe.br>
2020-10-28 15:49:11 -0300

    Adding base layout

--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -2,9 +2,12 @@ import P5 from 'p5';
 
 const sketch = (p5: P5) => {
   p5.setup = () => {
-    p5.createCanvas(1000, 1000);
+    const canvas = p5.createCanvas(1024, 768);
+    canvas.parent('canvas-container');
   };
-  p5.draw = () => {};
+  p5.draw = () => {
+    p5.background(255);
+  };
 };
 
 new P5(sketch);
